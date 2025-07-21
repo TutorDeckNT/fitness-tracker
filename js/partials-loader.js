2e4277b1b9483b742715625f1e3f554b42677da7
TutorDeckNT <prospertutorcorp@gmail.com>
2025-07-20 23:00:49 -0500

    Update partials-loader.js

--- js/partials-loader.js
+++ js/partials-loader.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const svgContainer = document.getElementById('svg-container');
     if (svgContainer) {
-        fetch('_partials/svg-symbols.html')
+        fetch('./_partials/svg-symbols.html')
             .then(response => response.text())
             .then(data => {
                 svgContainer.innerHTML = data;
