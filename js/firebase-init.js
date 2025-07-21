0415f0dafa850fdc15bbc3fdb8553d46a40713ed
TutorDeckNT <prospertutorcorp@gmail.com>
2025-07-21 00:12:00 -0500

    Delete js directory

--- js/firebase-init.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyDe-YVkiX81ISLhVfRdC0srkkHi2JQQeSs",
-  authDomain: "my-fitness-tracker-797e5.firebaseapp.com",
-  projectId: "my-fitness-tracker-797e5",
-  storageBucket: "my-fitness-tracker-797e5.firebasestorage.app",
-  messagingSenderId: "651004817600",
-  appId: "1:651004817600:web:3a277ea0126dfe0388bd25"
-};
