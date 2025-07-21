830da1376e7bacd3c2b451a6cc5b5cfce1150d6d
TutorDeckNT <prospertutorcorp@gmail.com>
2025-07-20 23:42:21 -0500

    Update partials-loader.js

--- js/partials-loader.js
+++ js/partials-loader.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
     if (svgContainer) {
         // This new path starts with a "/" to look from the website's root directory.
         // This is a more reliable way to find the file.
-        fetch('/_partials/svg-symbols.html')
+        fetch('_partials/svg-symbols.html')
             .then(response => {
                 if (!response.ok) {
                     console.error("CRITICAL ERROR: Could not load the icons file. Check the path and make sure the file exists at /_partials/svg-symbols.html");
