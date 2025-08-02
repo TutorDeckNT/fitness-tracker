8cdcb16d59cdeb51425faf6bd5e24008a297ae32
TutorDeckNT <prospertutorcorp@gmail.com>
2025-08-01 22:55:37 -0500

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,6 +1,4 @@
 // js/firebase-config.js
-
-// Your web app's Firebase configuration
 export const firebaseConfig = {
   apiKey: "AIzaSyDe-YVkiX81ISLhVfRdC0srkkHi2JQQeSs",
   authDomain: "my-fitness-tracker-797e5.firebaseapp.com",
@@ -9,12 +7,8 @@ export const firebaseConfig = {
   messagingSenderId: "651004817600",
   appId: "1:651004817600:web:3a277ea0126dfe0388bd25"
 };
-
-// Initialize Firebase if not already initialized
 if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
 }
-
-// Export the auth and firestore instances to be used in other files
 export const auth = firebase.auth();
 export const db = firebase.firestore();
